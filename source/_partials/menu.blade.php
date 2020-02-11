6dd1b1cd560722077f1671996de2ffd7bb062bf8
Lokman Musliu <lokman.musliu@gmail.com>
2020-02-11 20:59:08 +0100

    Responsive menu and updated packages

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -1,8 +1,41 @@
-<div class="h-24 bg-white shadow-lg flex items-center">
+<div class="bg-white shadow-lg flex items-center">
     <div class="container">
-        <div class="row justify-between">
-            <div class="col-2">
+        <div class="row py-6 justify-between">
+            <div class="col-6">
                 <p class="font-bold text-gray-800 text-2xl">Web Starter</p>
+            </div>
+
+            <div class="col-2 md:hidden block">
+                <label for="menu-toggle" class="cursor-pointer">
+                    <svg class="fill-current text-gray-800 w-8 h-8" xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 20 20">
+                        <title>menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+                    </svg>
+                </label>
+            </div>
+
+            <input class="hidden" type="checkbox" id="menu-toggle" />
+
+            <div id="menu" class="md:col-6 hidden md:flex justify-center md:justify-end">
+                <ul class="flex w-full p-5 flex-col md:w-auto md:p-0 md:flex-row items-center">
+                    <li class="w-full md:w-auto md:mr-6">
+                        <a href="#"
+                            class="block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent">Home</a>
+                    </li>
+                    <li class="w-full md:w-auto md:mr-6">
+                        <a href="#"
+                            class="block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent">About</a>
+                    </li>
+                    <li class="w-full md:w-auto md:mr-6">
+                        <a href="#"
+                            class="block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent">Pages</a>
+                    </li>
+                    <li class="w-full md:w-auto md:mr-6">
+                        <a href="#"
+                            class="block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent">Contact</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
